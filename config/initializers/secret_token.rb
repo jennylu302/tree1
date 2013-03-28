5492327a6b0f3a7fbe2778f5bde067294086956b
OpenShift guest <jennylu302@yahoo.com.tw>
2013-03-28 14:04:55 -0400

    Create a firt ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'c221a2c2a7c790ed75ba09a1d1743b6dbf22ecf37913d5963bbb0c4b3a502a34ae5fa9a3b90716f5c67106dc10b93d394e9f49bb951376195a2f7a95baa9a138'
